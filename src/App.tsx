b74423542223962cd0d45b127dfd96c2bced8521
Raí Vandenberg <rvdesigner20@gmail.com>
2025-06-07 00:28:32 -0300

    feat: fix import name for trash icon; update check icon SVG path; add Card and Container components with variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Text from "./components/text";
-import TrasnIcon from "./assets/icons/trash.svg?react"
+import TrashIcon from "./assets/icons/trash.svg?react"
 import Icon from "./components/icon";
 import CheckIcon from "./assets/icons/check.svg?react"
 import Pencil from "./assets/icons/pencil.svg?react"
@@ -10,36 +10,43 @@ import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
+import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
+import Container from "./components/container";
 
 
 export default function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <Text variant="body-md-bold">Olá</Text>
-      <div>
-        <Icon svg={TrasnIcon} className="fill-pink-base" />
-        <Icon svg={CheckIcon} className="fill-green-base" />
-        <Icon svg={Pencil} className="fill-blue-base" />
-        <Icon svg={Plus} className="fill-yellow-base" />
-        <Icon svg={Spinner} animate />
-        <Icon svg={X} className="fill-red-base" />
-      </div>
-      <div>
-        <Badge variant="primary" size="sm">Primary</Badge>
-        <Badge variant="secondary" size="sm">Secondary</Badge>
-      </div>
-      <div>
-        <Button className="mt-2" icon={Plus}>
-          Primary
-        </Button>
+    <Container>
+      <div >
+        <Text variant="body-md-bold">Olá</Text>
+        <div>
+          <Icon svg={TrashIcon} className="fill-pink-base" />
+          <Icon svg={CheckIcon} className="fill-green-base" />
+          <Icon svg={Pencil} className="fill-blue-base" />
+          <Icon svg={Plus} className="fill-yellow-base" />
+          <Icon svg={Spinner} animate />
+          <Icon svg={X} className="fill-red-base" />
+        </div>
+        <div>
+          <Badge variant="primary" size="sm">Primary</Badge>
+          <Badge variant="secondary" size="sm">Secondary</Badge>
+        </div>
+        <div>
+          <Button className="mt-2" icon={Plus}>
+            Primary
+          </Button>
 
-        <ButtonIcon icon={TrasnIcon} />
-        <ButtonIcon icon={TrasnIcon} variant="secondary" />
-        <ButtonIcon icon={TrasnIcon} variant="tertiary" />
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
+        <InputText />
+        <InputCheckbox />
+        <Card size="md">Oiiii</Card>
       </div>
-      <InputText />
-    </div>
+    </Container>
   )
 }
 
